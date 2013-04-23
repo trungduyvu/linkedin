fb2189a42af2a5d77ad1f23cbfcb231f6391552e
Trung Vu <trungduyvu@gmail.com>
2013-04-22 23:06:24 -0500

    bump version to 0.3.8.beta

--- lib/linked_in/version.rb
+++ lib/linked_in/version.rb
@@ -3,8 +3,8 @@ module LinkedIn
   module VERSION #:nodoc:
     MAJOR = 0
     MINOR = 3
-    PATCH = 7
-    PRE   = nil
+    PATCH = 8
+    PRE   = "beta"
     STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
   end
 
