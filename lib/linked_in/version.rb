01db541ad44fd03b64ab364d423e2fc6aaab3a58
Linh Vo <hailinh.vo@gmail.com>
2013-04-24 11:22:01 -0500

    change version

--- lib/linked_in/version.rb
+++ lib/linked_in/version.rb
@@ -3,8 +3,8 @@ module LinkedIn
   module VERSION #:nodoc:
     MAJOR = 0
     MINOR = 3
-    PATCH = 7
-    PRE   = nil
+    PATCH = 8
+    PRE   = 'beta2'
     STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
   end
 
