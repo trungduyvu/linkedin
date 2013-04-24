985cb77b07d86ebfed4b41a2cd55a4b34f7593ff
Linh Vo <hailinh.vo@gmail.com>
2013-04-24 11:53:47 -0500

    upgrade to version .beta3

--- lib/linked_in/helpers.rb
+++ lib/linked_in/helpers.rb
@@ -1,7 +1,7 @@
 module LinkedIn
   module Helpers
     autoload :Authorization, "linked_in/helpers/authorization"
-    autoload :Authorization2, "linked_in/helpers/authorization2"
+    autoload :Authorization2, "linked_in/helpers/oauth2"
     autoload :Request,       "linked_in/helpers/request"
   end
 end
--- lib/linked_in/version.rb
+++ lib/linked_in/version.rb
@@ -4,7 +4,7 @@ module LinkedIn
     MAJOR = 0
     MINOR = 3
     PATCH = 8
-    PRE   = 'beta2'
+    PRE   = 'beta3'
     STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
   end
 
